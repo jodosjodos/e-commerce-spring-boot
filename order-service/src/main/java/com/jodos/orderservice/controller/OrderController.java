29f4e75df532ab9f4d68da8a93aac0b1f63c9ba8
jodosjodos <jeandedieu2030@gmail.com>
2023-12-26 09:05:54 +0200

    configure  fallbackMethod

--- order-service/src/main/java/com/jodos/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/jodos/orderservice/controller/OrderController.java
@@ -2,6 +2,7 @@ package com.jodos.orderservice.controller;
 
 import com.jodos.orderservice.dto.OrderRequest;
 import com.jodos.orderservice.services.OrderService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,12 +10,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/order")
 @RequiredArgsConstructor
+@CircuitBreaker(name = "inventory" ,fallbackMethod = "fallbackMethod")
 public class OrderController {
-   private final OrderService service;
+    private final OrderService service;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public  String placeOrder(@RequestBody OrderRequest orderRequest){
+    public String placeOrder(@RequestBody OrderRequest orderRequest) {
         service.placeOrder(orderRequest);
-        return  " Order placed  successfully";
+        return " Order placed  successfully";
+    }
+    public  String fallbackMethod(OrderRequest orderRequest, RuntimeException exception){
+        return "oops! something went wrong , please order after some time";
     }
 }
